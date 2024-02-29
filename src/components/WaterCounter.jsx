2fa34bbe56c7744e1b742436d25bc79b5b8744ce
Mona Lisa <lisa-spencer@outlook.com>
2024-02-29 19:53:51 +0000

    added reset button to water counter

--- src/components/WaterCounter.jsx
+++ src/components/WaterCounter.jsx
@@ -4,18 +4,28 @@ import React, { useState } from 'react';
 function WaterCounter() {
     const [count, setCount] = useState(0)
 
+    const handleIncrement = () => {
+        setCount((prevCount) => prevCount + 1);
+    };
+
+    const handleReset = () => {
+        setCount(0);
+    };
+
     return (
         <>
         <div>
         <p>Click the button below to add to your water intake today!</p>
 
         <div className="WaterCounterCard">
-            <button onClick={() => setCount((count) => count + 1)}>
+            <button onClick={handleIncrement}>
             I've had {count} glasses of water today!
             </button>
         </div>
 
-        
+        <div className="WaterResetButton">
+            <button onClick={handleReset}> Reset </button>
+        </div>
 
         </div>
         </>
