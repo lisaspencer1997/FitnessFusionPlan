bbc87198396225066e4177438bb9b614d5c20e37
Mona Lisa <lisa-spencer@outlook.com>
2024-03-05 22:31:10 +0000

    added if else statements for days turning into hours, into minutes, into seconds

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -9,6 +9,47 @@ const CountdownTimer = () => {
     const [countdown, setCountdown] = useState('');
     const [isEventDay, setIsEventDay] = useState(false);
 
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setEventData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const loadEventFromLocalStorage = () => {
+        const storedEventData = localStorage.getItem('eventData');
+        if (storedEventData) {
+            setEventData(JSON.parse(storedEventData));
+        }
+    };
+
+    const calculateCountdown = () => {
+        const eventDate = new Date(eventData.eventDateTime);
+        const currentDate = new Date();
+
+        const timeDifference = eventDate - currentDate;
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        if (days > 0) {
+            setCountdown(`${days} days`);
+        } else if (hours > 0) {
+            setCountdown(`${hours} hours`)
+        } else if (minutes > 0) {
+            setCountdown(`${minutes} minutes`)
+        } else if (second > 0) {
+            setCountdown(`${seconds} seconds`)
+        } else {
+            setIsEventDay(true);
+            setCountdown('The day is finally here!');
+        };
+    };
+
+    useEffect
+
     return (
         <div>
             <p>Enter the details of the event:</p>
@@ -22,8 +63,14 @@ const CountdownTimer = () => {
                 placeholder="Date and Time"
                 value={eventData.eventDateTime}
             />
+
+            <div className="countdown-widget">
+                <p>{eventData.eventName}</p>
+                <p>{isEventDay ? 'Countdown:' : 'Days left:'}</p>
+                <p>{countdown}</p>
+            </div>
         </div>
-    )
+    );
     
 };
 
