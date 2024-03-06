45869245ac9d5551db98ca24968d01dbdb7b08c5
sonictrain <nicola.brucoli92@gmail.com>
2024-03-06 18:13:42 +0000

    added function to read file using FileReader according to MDN DOCS

--- src/components/AvatarComponent.jsx
+++ src/components/AvatarComponent.jsx
@@ -10,6 +10,24 @@ const AvatarComponent = () => {
     const image = event.target.files[0];
   };
 
+  // function to convert image to base64
+  //! code from MDN WEB DOCS https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader/load_event
+  const image2base64 = (image) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+
+      reader.onerror = (error) => {
+        reject(error);
+      };
+
+      reader.readAsDataURL(image);
+    });
+  };
+
   // input file component
   return (
       <Input />
