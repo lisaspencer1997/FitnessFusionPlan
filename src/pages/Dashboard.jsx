2d3eb9d94b58703d0c0344532f41c638b8e6c155
sonictrain <nicola.brucoli92@gmail.com>
2024-03-07 00:28:56 +0000

    Added tailwind grid and subgrid for better layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,31 +11,50 @@ import {
 
 const Dashboard = () => {
     return (
-        <div className='flex flex-col gap-2 h-full'>
-            <Header heading='Dashboard' />
-            <div className='w-full flex flex-row gap-2 mt-2 text-center'>
-                <Card className='flex-1 flex flex-col content-center'>
-                    <Typography variant="h5" color="blue-gray">
-                        Widget 1
-                    </Typography>
-                </Card>
-                <Card className='flex-1'>
-                    <Countdown />
-                </Card>
-                
-                <Card className='flex-1'>
-                    <Typography variant="h5" color="blue-gray">
-                        Widget 3
-                    </Typography>
-                </Card>
+        // Container grid
+        <div className='grid grid-cols-6 gap-4'>
+
+            {/* Header */}
+            <div className='col-start-1 col-span-6'>
+                <Header heading='Dashboard' />
             </div>
-            <div className='flex flex-row gap-2 h-full'>
-                <Card className='flex-1 content-stretch'>
+
+            {/* Widget Next Activity */}
+            <Card className='col-start-1 col-span-2'>
+                <Typography variant="h5" color="blue-gray">
+                    Widget 1
+                </Typography>
+            </Card>
+
+            {/* Widget Next Milestone */}
+            <Card className='col-start-3 col-span-2'>
+                <Countdown />
+            </Card>
+
+            {/* Widget Next Food */}
+            <Card className='col-start-5 col-span-2'>
+                <Typography variant="h5" color="blue-gray">
+                    Widget 3
+                </Typography>
+            </Card>
+
+            {/* Nutri Check Card */}
+            <Card className='col-start-1 col-span-3'>
+                <Typography variant="h5" color="blue-gray" className="">
+                    Nutri Check
+                </Typography>
+            </Card>
+
+            <div className='col-span-3 grid grid-rows-subgrid gap-4 row-span-2'>
+                {/* Recipe Feed */}
+                <Card>
                     <Typography variant="h5" color="blue-gray" className="">
                         Nutri Check
                     </Typography>
                 </Card>
-                <Card className='flex-1 content-stretch'>
+
+                {/* Fitness Carousel */}
+                <Card className='flex-1'>
                     <FitnessCarousel />
                 </Card>
             </div>
