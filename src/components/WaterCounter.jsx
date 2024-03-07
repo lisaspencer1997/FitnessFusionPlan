db69cc75e595c40c358ab70ae8ff89b191b5754e
sonictrain <nicola.brucoli92@gmail.com>
2024-03-07 00:28:17 +0000

    Added comments and button group as component

--- src/components/WaterCounter.jsx
+++ src/components/WaterCounter.jsx
@@ -1,34 +1,47 @@
 import React, { useEffect, useState } from 'react';
+import {
+    ButtonGroup,
+    Button,
+} from "@material-tailwind/react";
 
+import {
+    PlusCircleIcon,
+    MinusCircleIcon,
+} from "@heroicons/react/24/solid";
 
 function WaterCounter() {
 
+    // get the counter from the localStorage
     const initialCount = parseInt(localStorage.getItem('waterCount')) || 0;
 
     const [count, setCount] = useState(initialCount)
 
+    // increment counter
     const handleIncrement = () => {
-        const newCount = count + 1;
-        setCount(newCount);
+        const newCount = count + 0.2;
+        setCount(Math.round(newCount * 100) / 100);
         localStorage.setItem('waterCount', newCount.toString());
     };
 
+    // decrement counter
     const handleDecrement = () => {
-        const newCount = Math.max(0, count - 1);
-        setCount(newCount);
+        const newCount = Math.max(0, count - 0.2);
+        setCount(Math.round(newCount * 100) / 100);
         localStorage.setItem('waterCount', newCount.toString());
     };
 
+    // reset counter at midnight
     const handleReset = () => {
         setCount(0);
         localStorage.setItem('waterCount', '0')
     };
 
+    // check for the time and call reset function if midnight
     useEffect(() => {
         const now = new Date();
 
         const midnight = new Date(now)
-        midnight.setHours(24,0,0,0)
+        midnight.setHours(24, 0, 0, 0)
         const timeUntilMidnight = midnight - now
 
         const timeoutId = setTimeout(() => {
@@ -39,18 +52,12 @@ function WaterCounter() {
     }, [count])
 
     return (
-        <div className="WaterCounter">
-        
-        <p className="WaterCounterDisplay">I've had {count} glasses of water today!</p>
-
-        <div className="WaterButtons">
-            <button onClick={handleDecrement}></button>
-            <button onClick={handleIncrement}></button>
-        </div>
-
-
-        </div>
+        <ButtonGroup fullWidth={true} className='mt-3' color="blue">
+            <Button onClick={handleDecrement}><MinusCircleIcon /></Button>
+            <Button disabled={true}>{count} L</Button>
+            <Button onClick={handleIncrement}><PlusCircleIcon /></Button>
+        </ButtonGroup>
     )
 }
 
-export default WaterCounter
+export default WaterCounter;
