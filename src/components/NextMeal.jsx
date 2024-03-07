5fd0e8a426ede760e15e2f12e1f84fe4e3dc19b0
sonictrain <nicola.brucoli92@gmail.com>
2024-03-07 00:59:07 +0000

    Added NextMeal Component

---
+++ src/components/NextMeal.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const NextMeal = () => {
+    return (
+        <div>
+            {/* MealPlan component with Props */}
+        </div>
+    )
+}
+
+export default NextMeal;
